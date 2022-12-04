c8e3bcc74ae7d435dd9366e505b7e254e6696c6d
Cyrille Polier <cyrille.polier@he-arc.ch>
2022-12-04 16:57:54 +0100

    fix typo

--- memecard/settings.py
+++ memecard/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'memecard-app',
+    'memecard_app',
 ]
 
 MIDDLEWARE = [
--- memecard_app/views.py
+++ memecard_app/views.py
@@ -3,9 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'memecard-app/index.html')
+    return render(request, 'memecard_app/index.html')
 
 def about(request):
-    return render(request, 'memecard-app/about.html')
+    return render(request, 'memecard_app/about.html')
 
 
