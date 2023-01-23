01f2521e7605d960deafa29377a8cbebb6e4fda2
Cyrille Polier <cyrille.polier@he-arc.ch>
2023-01-23 12:18:48 +0100

    static file support

--- memecard/urls.py
+++ memecard/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views
+from django.conf import settings
+from django.conf.urls.static import static
 from memecard_app.forms import UserLoginForm
 
 urlpatterns = [
